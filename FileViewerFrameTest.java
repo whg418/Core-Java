d555dfb5b0bacb697e49ab9e22bd3836418751d8
woohoogoo <39509806+whg418@users.noreply.github.com>
2019-03-01 15:43:17 +0800

    Create FileViewerFrameTest.java

---
+++ FileViewerFrameTest.java
@@ -0,0 +1,14 @@
+package fileChooser;
+
+import java.awt.EventQueue;
+
+public class FileViewerFrameTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		EventQueue.invokeLater(() -> {
+			new ImageViewerFrame().setVisible(true);
+		});
+	}
+
+}
